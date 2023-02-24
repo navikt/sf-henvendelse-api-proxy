ed3b792196e6f618a0b002bfeafd5ef8ad24e9c6
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2023-02-24 08:38:15 +0100

    Tidy up time log, see if a sum adds up to postman stat

--- src/main/kotlin/no/nav/sf/henvendelse/api/proxy/token/FetchStats.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/api/proxy/token/FetchStats.kt
@@ -8,17 +8,21 @@ object FetchStats {
     var elapsedTimeAccessTokenRequest: Long = -1L
     var elapsedTimeOboExchangeRequest: Long = -1L
     var elapsedTimeTokenValidation: Long = -1L
+    var latestCallElapsedTime: Long = -1L
 
     fun resetFetchVars(callTime: Long) {
         log.info { "($callTime) stats reset" }
         elapsedTimeAccessTokenRequest = 0L
         elapsedTimeOboExchangeRequest = 0L
         elapsedTimeTokenValidation = 0L
+        latestCallElapsedTime = 0L
     }
     var callElapsedTime: MutableMap<String, Long> = mutableMapOf()
 
     fun logStats(callTime: Long) {
-        log.info { "($callTime) Validation ${elapsedTimeTokenValidation }Accesstoken: $elapsedTimeAccessTokenRequest, OboExchange $elapsedTimeOboExchangeRequest" }
-        log.info { "Call times $callElapsedTime" }
+        log.info { "($callTime) : Validation $elapsedTimeTokenValidation, Accesstoken: $elapsedTimeAccessTokenRequest," +
+                " OboExchange $elapsedTimeOboExchangeRequest, Call $latestCallElapsedTime," +
+                " Sum ${elapsedTimeTokenValidation + elapsedTimeAccessTokenRequest + elapsedTimeOboExchangeRequest + latestCallElapsedTime}" }
+        log.info { "Call times per path stub: $callElapsedTime" }
     }
 }
