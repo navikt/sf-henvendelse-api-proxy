307b97d3643c8d3936ff4bf4dd9a06be60c07169
Björn Hägglund <bjorn.hagglund@nav.no>
2025-06-27 13:54:00 +0200

    Try http protocol 1_1 on proxy2

--- src/main/kotlin/no/nav/sf/henvendelse/api/proxy/httpclient/supportProxy.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/api/proxy/httpclient/supportProxy.kt
@@ -3,6 +3,7 @@ package no.nav.sf.henvendelse.api.proxy.httpclient
 import no.nav.sf.henvendelse.api.proxy.env
 import no.nav.sf.henvendelse.api.proxy.env_HTTPS_PROXY
 import okhttp3.OkHttpClient
+import okhttp3.Protocol
 import org.http4k.client.OkHttp
 import org.http4k.core.HttpHandler
 import java.io.File
@@ -15,6 +16,7 @@ private fun createOkHttpClient(proxy: Proxy? = null): OkHttpClient {
         .apply {
             proxy?.let { this.proxy(it) }
         }
+        .protocols(listOf(Protocol.HTTP_1_1))
         .connectTimeout(Duration.ofSeconds(20))
         .readTimeout(Duration.ofSeconds(20))
         .writeTimeout(Duration.ofSeconds(20))
