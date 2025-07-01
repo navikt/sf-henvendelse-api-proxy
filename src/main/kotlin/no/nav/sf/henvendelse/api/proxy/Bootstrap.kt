beed97305740ef5cece5f47e25a902f0aa7eb5f2
Björn Hägglund <bjorn.hagglund@nav.no>
2025-07-01 08:09:26 +0200

    Try lateinit dev-fss

--- src/main/kotlin/no/nav/sf/henvendelse/api/proxy/Bootstrap.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/api/proxy/Bootstrap.kt
@@ -1,8 +1,9 @@
 package no.nav.sf.henvendelse.api.proxy
 
-val application = Application()
+lateinit var application: Application
 
 fun main() {
     Thread.sleep(3000)
+    application = Application()
     application.start()
 }
