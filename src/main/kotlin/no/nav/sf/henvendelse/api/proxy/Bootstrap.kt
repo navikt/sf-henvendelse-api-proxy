3b303949eae874ddc37985e15f24bfba334838a6
Björn Hägglund <bjorn.hagglund@nav.no>
2025-07-01 08:33:37 +0200

    2s

--- src/main/kotlin/no/nav/sf/henvendelse/api/proxy/Bootstrap.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/api/proxy/Bootstrap.kt
@@ -5,7 +5,7 @@ lateinit var application: Application
 fun main() {
     // Suspecting potential race condition for load of web proxy envs and instantiating of token validator.
     // Therefor a small delay on boot
-    Thread.sleep(1000)
+    Thread.sleep(2000)
     application = Application()
     application.start()
 }
